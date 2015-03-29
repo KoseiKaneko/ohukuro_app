733d4ad70389e6ab46a4941964c21ced5c18d57d
金子幸生 <kosei.898989@gmail.com>
2015-03-29 18:41:59 +0900

    first commit

---
+++ app/controllers/main_controller.rb
@@ -0,0 +1,24 @@
+class MainController < ApplicationController
+  def top
+  end
+  def usersignup
+  end
+  def login
+  end
+  def index
+  end
+  def detail
+  end
+  def confirm
+  end
+
+  def profile
+  end
+
+  def makemenu
+  end
+  def perfect
+  end
+  def dashboard
+  end
+end
